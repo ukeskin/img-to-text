0cc9bcae5e269a327bf8178517d87e44d2e11bed
Umut Keskin <ukeskin2626@gmail.com>
2023-08-15 23:43:33 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const Tesseract = require("tesseract.js");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
+
+app.post("/", async (req, res) => {
+  const base64Data = req.body.base64.replace(/^data:image\/png;base64,/, "");
+  const url = `data:image/png;base64,${base64Data}`;
+
+  try {
+    Tesseract.recognize(url, "tur", {
+      logger: (m) => console.log(m),
+    }).then(({ data: { text } }) => {
+      console.log(text);
+      res.json(text);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "OCR işlemi sırasında bir hata oluştu." });
+  }
+});
+
+module.exports = app;
